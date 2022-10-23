1b8350e900f5503b3ac20c7bb17ddb62f9e997b2
Ahmed-Abdelrhim <aabdelrhim974@gmail.com>
2022-10-23 05:46:35 +0200

    udapte

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -15,16 +15,28 @@
                         @endif
 
                         @guest('author')
-                            <div>Not Authenticated In Author Guard </div>
+                            <div>Not Authenticated In Author Guard</div>
                         @else
                             <div>{{Auth::guard('author')->user()->name}}</div>
                         @endguest
-{{--                            <div>Checking Here {{Auth::guard('author')->check()}}</div>--}}
+                        {{--                            <div>Checking Here {{Auth::guard('author')->check()}}</div>--}}
                         {{ __('You are logged in!') }}
-{{--                        <div>--}}
-{{--                            <video src="{{asset('storage/third/1665768905.mp4')}}" type="mp4" controlsList="nodownload" controls >--}}
-{{--                            </video>--}}
-{{--                        </div>--}}
+                        {{--                        <div>--}}
+                        {{--                            <video src="{{asset('storage/third/1665768905.mp4')}}" type="mp4" controlsList="nodownload" controls >--}}
+                        {{--                            </video>--}}
+                        {{--                        </div>--}}
+
+                    </div>
+                </div>
+                {{-- My Card--}}
+                <div class="card mt-3" >
+                    <div class="card-body">
+                        <h5 class="card-title">{{__('msg.welcome', ['name' => Auth::guard('author')->user()->name]) }}</h5>
+                        <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of
+                            the card's content.</p>
+                        <a href="#" class="card-link">Card link</a>
+                        <a href="#" class="card-link">Another link</a>
                     </div>
                 </div>
             </div>
