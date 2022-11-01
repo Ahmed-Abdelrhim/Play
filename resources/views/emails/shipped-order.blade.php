ae317ffb010a093533f79f5d1224a3ce6c938bc9
Ahmed-Abdelrhim <aabdelrhim974@gmail.com>
2022-11-01 19:23:24 +0200

    update

--- resources/views/emails/shipped-order.blade.php
+++ resources/views/emails/shipped-order.blade.php
@@ -1,13 +1,3 @@
-{{--@extends('layouts.app')--}}
-{{--@section('content')--}}
-{{--    <div class="container">--}}
-{{--        <div class="row col-6 mx-auto">--}}
-            Your Order Was Shipped To The Shipping Company It Will Arrive Very Soon So Be There And Wait For A Phone Call To Receive Your Order.
-{{--        </div>--}}
-{{--    </div>--}}
-{{--@endsection--}}
-
-
 @component('mail::message')
     Your order was shipped  wait for a phone call to receive it.
     @component('mail::button',['url' => 'https://www.google.com'])
