e616e1c6207752514c8c8ad8ca4162ab2e671027
Ahmed-Abdelrhim <aabdelrhim974@gmail.com>
2022-12-30 18:44:23 +0200

    update

--- app/Exports/UserExport.php
+++ app/Exports/UserExport.php
@@ -4,8 +4,10 @@ namespace App\Exports;
 
 use App\Models\Author;
 use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\ShouldAutoSize;
+use Maatwebsite\Excel\Concerns\WithMapping;
 
-class UserExport implements FromCollection
+class UserExport implements FromCollection , ShouldAutoSize
 {
     /**
     * @return \Illuminate\Support\Collection
