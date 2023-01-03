6f3de642bbed3265388a15822be0d9f60de64abc
Ahmed-Abdelrhim <aabdelrhim974@gmail.com>
2023-01-03 10:46:53 +0200

    update

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,7 @@ use App\Http\Controllers\ProblemsController;
 use App\Http\Controllers\Files\ImagesController;
 use App\Http\Controllers\SpatieController;
 use App\Http\Controllers\Files\FilesController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -29,162 +30,146 @@ use App\Http\Controllers\Files\FilesController;
 Route::group(['middleware' => 'disable_back_btn'], function () {
 
 
-
-Route::get('not-found-page', [PlayController::class, 'errorPage'])->name('error');
+    Route::get('not-found-page', [PlayController::class, 'errorPage'])->name('error');
 //Route::get('s3',function (){
 //    return view('s3');
 //});
 
-Route::get('/', function () {
-    return view('welcome');
-});
-Route::get('play/{id}', [PlayController::class, 'play']);
-Route::get('solve',[BlogPostController::class,'problemSolving']);
-
-Route::group(['middleware' => 'guest:author'], function () {
-    Route::get('login', [CustomLoginController::class, 'showLoginForm'])->name('login');
-    Route::post('login', [CustomLoginController::class, 'login'])->name('signIn')->middleware('guest');
-    Route::post('logout', [CustomLoginController::class, 'logout']);
-    Route::get('register/now', [CustomLoginController::class, 'showRegisterForm'])->name('register');
-    Route::post('register/To/Login', [CustomLoginController::class, 'register'])->name('register.post');
-});
-
-
-Route::get('play/{id}', [PlayController::class, 'play']);
-
-
-Route::group(['middleware' => 'auth:author'], function () {
-    Route::post('logout', [CustomLoginController::class, 'logout'])->name('logout');
-    Route::get('/home', [HomeController::class, 'index'])->name('home');
-
-
-    Route::get('payment', [PaymentController::class, 'showPaymentForm'])->name('pay');
-    Route::get('restore/post/{id}', [PlayController::class, 'restoreBlogPosts']);
-    Route::get('all/posts', [PlayController::class, 'showAllPosts'])->name('blog_posts');
-    Route::get('posts/{id}', [PlayController::class, 'showPosts'])->name('show.blog_post.by.id');
-
-    Route::get('show/dataTables/blogposts',[LearnController::class,'showDataTablesIndex'])->name('dataTables')
-    ->middleware('permission:edit post')
-    ;
-    Route::get('get/dataTables/blogposts/all',[LearnController::class,'getDataTablesIndex'])->name('dataTables.all');
-
-
-    Route::get('add/blogPost', [PlayController::class, 'showBlogPostForm'])->name('add.blog_post')
-    ->middleware('permission:write post')
-    ;
-    Route::get('adding/blogPost', [PlayController::class, 'addBlogPost'])->name('create.blogPost');
-    Route::get('update/post/{id}', [PlayController::class, 'updateBlogPostForm'])->name('update.post.form');
-    Route::post('update/post/{id}', [PlayController::class, 'updateBlogPost'])->name('update.post');
-    Route::delete('delete/post/{id}', [PlayController::class, 'destroy'])->name('delete.post');
-
-    Route::get('test', function () {
-        return view('test');
+    Route::get('/', function () {
+        return view('welcome');
     });
-    Route::get('test/test', [])->name('stripe.post');
+    Route::get('play/{id}', [PlayController::class, 'play']);
+    Route::get('solve', [BlogPostController::class, 'problemSolving']);
 
-    Route::get('email-send',[MailController::class,'viewSendEmailForm'])->name('sending-email');
-    Route::post('send/message', [MailController::class, 'sendEmail'])->name('send.gmail');
-    Route::get('check-verified-code',[MailController::class,'checkVerificationCodeForm'])->name('check.verification.code');
-    Route::post('check-mobile-number',[MailController::class,'checkMobileNumber'])->name('check.mobile_no');
-    Route::post('check/verification/code',[MailController::class,'checkVerificationCode'])->name('check.code');
-
-    Route::get('play/with/livewire', function () {
-        return view('play');
-    })->name('livewire');
-
-    Route::get('callback', function () {
-        return 'Success';
-    });
-    Route::get('error', function () {
-        return 'Error';
+    Route::group(['middleware' => 'guest:author'], function () {
+        Route::get('login', [CustomLoginController::class, 'showLoginForm'])->name('login');
+        Route::post('login', [CustomLoginController::class, 'login'])->name('signIn')->middleware('guest');
+        Route::post('logout', [CustomLoginController::class, 'logout']);
+        Route::get('register/now', [CustomLoginController::class, 'showRegisterForm'])->name('register');
+        Route::post('register/To/Login', [CustomLoginController::class, 'register'])->name('register.post');
     });
 
-    Route::get('show/upload', [PlayController::class, 'uploadForm'])->name('upload.form');
-    Route::post('upload', [PlayController::class, 'upload'])->name('upload');
 
-    Route::get('user/profile', [PlayController::class, 'viewProfilePage'])->name('profile');
-    Route::post('save/profile/data', [PlayController::class, 'storeUserProfileData'])->name('post.profile.data');
-    Route::post('download/profile/image',[SpatieController::class,'downloadImageProfile'])->name('download.profile.image');
-
-    Route::get('spatie',[SpatieController::class,'handle'])->name('spatie-media');
+    Route::get('play/{id}', [PlayController::class, 'play']);
 
 
-    ################### Custom Play  ###################
-
-    Route::get('all-posts',[BlogPostController::class,'index'])->name('all-posts');
-    Route::get('show/{id}',[BlogPostController::class,'show'])->name('only.show');
-    Route::post('update/{id}',[BlogPostController::class,'update'])->name('only.update');
-
-    Route::get('add/comment',[CommentsController::class,'addCommentForm'])->name('add.comment');
-    Route::post('store/comment',[CommentsController::class,'storeComment'])->name('store.comment');
-
-    Route::get('excel',[LearnController::class,'playWithExcel']);
-
-    Route::get('playy',[LearnController::class,'play'])->middleware('can:play');
-    Route::get('problem',[ProblemsController::class,'solveFirst']);
-    Route::get('temp',[ProblemsController::class,'tempProblem']);
-    Route::get('strings',[ProblemsController::class,'strings']);
-
-    Route::get('many',[ImagesController::class,'uploadForm']);
-
-    Route::post('upload-multiple',[ImagesController::class,'uploadMultipleImages'])->name('multiple.images');
-
-});
-Route::get('hash', function () {
-    if (session()->has('locale')) {
-        return 'Yes';
-    } else {
-        return 'No';
-    }
-    $locale = session()->get('locale');
-    $locale2 = session()->get('locale2');
-    return 'Locale => ' . $locale . '<br> Locale 2 => ' . $locale2;
-    // return bcrypt('12345678');
-    //    session()->put('locale','ar');
-    //    app()->setLocale('locale');
-    // session()->get('ss');
-    // return app()->getLocale();
-});
-
-Route::get('sweet',[ImagesController::class,'sweet']);
-
-Route::get('most/active/last/month', [PlayController::class, 'activeLastMonthAuthor'])->name('most.active.last.month');
-
-    Route::get('send/gmail',[MailController::class,'sendMailForm']);
-Route::post('send/gmail/msg',[MailController::class,'send'])->name('email.send');
+    Route::group(['middleware' => 'auth:author'], function () {
+        Route::post('logout', [CustomLoginController::class, 'logout'])->name('logout');
+        Route::get('/home', [HomeController::class, 'index'])->name('home');
 
 
+        Route::get('payment', [PaymentController::class, 'showPaymentForm'])->name('pay');
+        Route::get('restore/post/{id}', [PlayController::class, 'restoreBlogPosts']);
+        Route::get('all/posts', [PlayController::class, 'showAllPosts'])->name('blog_posts');
+        Route::get('posts/{id}', [PlayController::class, 'showPosts'])->name('show.blog_post.by.id');
 
-Route::get('send/sms',[MailController::class,'sendSms'])->name('send.sms');
-Route::get('download',[SpatieController::class,'download']);
+        Route::get('show/dataTables/blogposts', [LearnController::class, 'showDataTablesIndex'])->name('dataTables')
+            ->middleware('permission:edit post');
+        Route::get('get/dataTables/blogposts/all', [LearnController::class, 'getDataTablesIndex'])->name('dataTables.all');
 
-Route::get('export',[FilesController::class,'export'])->name('export.excel');
 
-Route::get('sub',[FilesController::class,'subMonth'])->name('sub');
+        Route::get('add/blogPost', [PlayController::class, 'showBlogPostForm'])->name('add.blog_post')
+            ->middleware('permission:write post');
+        Route::get('adding/blogPost', [PlayController::class, 'addBlogPost'])->name('create.blogPost');
+        Route::get('update/post/{id}', [PlayController::class, 'updateBlogPostForm'])->name('update.post.form');
+        Route::post('update/post/{id}', [PlayController::class, 'updateBlogPost'])->name('update.post');
+        Route::delete('delete/post/{id}', [PlayController::class, 'destroy'])->name('delete.post');
+
+        Route::get('test', function () {
+            return view('test');
+        });
+        Route::get('test/test', [])->name('stripe.post');
+
+        Route::get('email-send', [MailController::class, 'viewSendEmailForm'])->name('sending-email');
+        Route::post('send/message', [MailController::class, 'sendEmail'])->name('send.gmail');
+        Route::get('check-verified-code', [MailController::class, 'checkVerificationCodeForm'])->name('check.verification.code');
+        Route::post('check-mobile-number', [MailController::class, 'checkMobileNumber'])->name('check.mobile_no');
+        Route::post('check/verification/code', [MailController::class, 'checkVerificationCode'])->name('check.code');
+
+        Route::get('play/with/livewire', function () {
+            return view('play');
+        })->name('livewire');
+
+        Route::get('callback', function () {
+            return 'Success';
+        });
+        Route::get('error', function () {
+            return 'Error';
+        });
+
+        Route::get('show/upload', [PlayController::class, 'uploadForm'])->name('upload.form');
+        Route::post('upload', [PlayController::class, 'upload'])->name('upload');
+
+        Route::get('user/profile', [PlayController::class, 'viewProfilePage'])->name('profile');
+        Route::post('save/profile/data', [PlayController::class, 'storeUserProfileData'])->name('post.profile.data');
+        Route::post('download/profile/image', [SpatieController::class, 'downloadImageProfile'])->name('download.profile.image');
+
+        Route::get('spatie', [SpatieController::class, 'handle'])->name('spatie-media');
+
+
+        ################### Custom Play  ###################
+
+        Route::get('all-posts', [BlogPostController::class, 'index'])->name('all-posts');
+        Route::get('show/{id}', [BlogPostController::class, 'show'])->name('only.show');
+        Route::post('update/{id}', [BlogPostController::class, 'update'])->name('only.update');
+
+        Route::get('add/comment', [CommentsController::class, 'addCommentForm'])->name('add.comment');
+        Route::post('store/comment', [CommentsController::class, 'storeComment'])->name('store.comment');
+
+        Route::get('excel', [LearnController::class, 'playWithExcel']);
+
+        Route::get('playy', [LearnController::class, 'play'])->middleware('can:play');
+        Route::get('problem', [ProblemsController::class, 'solveFirst']);
+        Route::get('temp', [ProblemsController::class, 'tempProblem']);
+        Route::get('strings', [ProblemsController::class, 'strings']);
+
+        Route::get('many', [ImagesController::class, 'uploadForm']);
+
+        Route::post('upload-multiple', [ImagesController::class, 'uploadMultipleImages'])->name('multiple.images');
+
+    });
+    Route::get('hash', function () {
+        return bcrypt('12345678');
+    });
+
+    Route::get('sweet', [ImagesController::class, 'sweet']);
+
+    Route::get('most/active/last/month', [PlayController::class, 'activeLastMonthAuthor'])->name('most.active.last.month');
+
+    Route::get('send/gmail', [MailController::class, 'sendMailForm']);
+    Route::post('send/gmail/msg', [MailController::class, 'send'])->name('email.send');
+
+
+    Route::get('send/sms', [MailController::class, 'sendSms'])->name('send.sms');
+    Route::get('download', [SpatieController::class, 'download']);
+
+    Route::get('export', [FilesController::class, 'export'])->name('export.excel');
+
+    Route::get('sub', [FilesController::class, 'subMonth'])->name('sub');
 
 
 // Language
-    Route::get('language/{locale}',[HomeController::class,'change_locale'])->name('change_locale');
+    Route::get('language/{locale}', [HomeController::class, 'change_locale'])->name('change_locale');
 
-/*
- * ----------------------------------------------------------------------------------------------------
- * -                         -                                    -                                   -
- * -                         -                                    -                                   -
- * -                         -                                    -                                   -
- * -                         -                                    -                                   -
- * -                         -                                    -                                   -
- * -                         -                                    -                                   -
- * -                         -                                    -                                   -
- * -                         -                                    -                                   -
- * ----------------------------------------------------------------------------------------------------
- * -                         -                                    -                                   -
- * -                         -                                    -                                   -
- * -                         -                                    -                                   -
- * -                         -                                    -                                   -
- * -                         -                                    -                                   -
- * -                         -                                    -                                   -
- * -                         -                                    -                                   -
- * -                         -                                    -                                   -                                    -                                   -
- * ----------------------------------------------------------------------------------------------------
- */
+    /*
+     * ----------------------------------------------------------------------------------------------------
+     * -                         -                                    -                                   -
+     * -                         -                                    -                                   -
+     * -                         -                                    -                                   -
+     * -                         -                                    -                                   -
+     * -                         -                                    -                                   -
+     * -                         -                                    -                                   -
+     * -                         -                                    -                                   -
+     * -                         -                                    -                                   -
+     * ----------------------------------------------------------------------------------------------------
+     * -                         -                                    -                                   -
+     * -                         -                                    -                                   -
+     * -                         -                                    -                                   -
+     * -                         -                                    -                                   -
+     * -                         -                                    -                                   -
+     * -                         -                                    -                                   -
+     * -                         -                                    -                                   -
+     * -                         -                                    -                                   -                                    -                                   -
+     * ----------------------------------------------------------------------------------------------------
+     */
 });
