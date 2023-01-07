2f708fa862efa4d4b4cfede9b8ff97f5d4624f00
Ahmed-Abdelrhim <aabdelrhim974@gmail.com>
2023-01-07 17:35:57 +0200

    update

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -64,27 +64,3 @@ class PaymentController extends Controller
     }
 
 }
-
-//Fill optional data
-//'MobileCountryCode'  => '+965',
-//'CustomerMobile'     => '1234567890',
-
-//'CustomerReference'  => 'orderId',
-//'CustomerCivilId'    => 'CivilId',
-//'UserDefinedField'   => 'This could be string, number, or array',
-//'ExpiryDate'         => '', //The Invoice expires after 3 days by default. Use 'Y-m-d\TH:i:s' format in the 'Asia/Kuwait' time zone.
-//'SourceInfo'         => 'Pure PHP', //For example: (Symfony, CodeIgniter, Zend Framework, Yii, CakePHP, etc)
-//'CustomerAddress'    => $customerAddress,
-//'InvoiceItems'       => $invoiceItems,
-
-
-//            "CustomerName" => "Ahmed",
-//            "NotificationOption" => "Lnk",
-//            "MobileCountryCode" => "20",
-//            "CustomerMobile" => "01152067271",
-//            "CustomerEmail" => "aabdelrhim974@gmail.com",
-//            "InvoiceValue" => 100,
-//            "DisplayCurrencyIso" => "EGP",
-//            "CallBackUrl" => 'https://google.com',
-//            "ErrorUrl" => 'https://youtube.com',
-//            "Language" => "en",
