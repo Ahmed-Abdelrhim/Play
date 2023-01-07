979817f2e1c78d5cb1e160fab65923437989497b
Ahmed-Abdelrhim <aabdelrhim974@gmail.com>
2023-01-07 17:56:33 +0200

    update

---
+++ app/Models/Category.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Category extends Model
+{
+    use HasFactory;
+    public $table = 'categories';
+    protected $fillable = ['name_en','name_ar','image'];
+    protected $hidden = [];
+
+    protected $casts = [];
+}
---
+++ database/migrations/2023_01_07_154344_categories.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Categories extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up(): void
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name_en');
+            $table->string('name_ar');
+            $table->string('image')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('categories');
+    }
+}
