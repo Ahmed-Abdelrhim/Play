4e67b393c6bd2102a1dbf99ede0a34b7162ae208
Ahmed-Abdelrhim <aabdelrhim974@gmail.com>
2023-01-12 10:37:38 +0200

    update

--- app/Models/Cart.php
+++ app/Models/Cart.php
@@ -11,12 +11,17 @@ class Cart extends Model
     use HasFactory;
 
     public $table = 'cart';
+
     protected $guarded = [];
 
     protected $hidden = [];
-    protected $casts = [];
 
-    public function product(): BelongsTo
+    protected $casts = [
+        'created_at' => 'date:Y-m-d',
+        'updated_at' => 'date:Y-m-d',
+    ];
+
+    public function product():BelongsTo
     {
         return $this->belongsTo(Product::class,'product_id','id');
     }
